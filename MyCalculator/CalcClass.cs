28373c4556592d8d3bf3ffe5bae4d87c0c567cae
Kukikoe <ja.bondareva2014@yandex.ru>
2018-05-06 23:28:34 +0300

    first commit

---
+++ MyCalculator/CalcClass.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyCalculator
+{
+    class CalcClass
+    {
+        public double Plus(double a, double b)
+        {
+            return a + b;
+        }
+        public double Multiply(double a, double b)
+        {
+            return a * b;
+        }
+        public double Minus(double a, double b)
+        {
+            return a - b;
+        }
+        public double Div(double a, double b)
+        {
+            return a / b;
+        }
+    }
+}
